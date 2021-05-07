578658a8c2cb8fe1b2a81ce3ee4b3da92a32fbaa
Andreas Pieger <andreas.pieger@epg.com>
2021-05-07 06:44:41 +0200

    Solve first chapter

--- tutorial/1_CppGrundlagen/Main.cc
+++ tutorial/1_CppGrundlagen/Main.cc
@@ -2,7 +2,24 @@
 
 int main(void)
 {
-    std::cout << "Hello World!" << std::endl;
+    int zahl1;
+    int zahl2;
+    int ergebnis;
+
+    zahl1 = 4;
+    zahl2 = 3;
+
+    ergebnis = zahl1 * zahl2;
+    std::cout << "Ergebnis Multiplikation: " << ergebnis << std::endl;
+
+    ergebnis = zahl1 + zahl2;
+    std::cout << "Ergebnis Addition: " << ergebnis << std::endl;
+
+    ergebnis = zahl1 - zahl2;
+    std::cout << "Ergebnis Subtratkion: " << ergebnis << std::endl;
+
+    ergebnis = zahl1 / zahl2;
+    std::cout << "Ergebnis Division: " << ergebnis << std::endl;
 
     return 0;
 }
